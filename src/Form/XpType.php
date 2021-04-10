65769a34195c67ab3b5a802f44a9eed7ab0559d1
Michael Krysztofiak <mkl@iMac-de-Michael.local>
2021-04-10 16:21:09 +0200

    maj _form xp

--- src/Form/XpType.php
+++ src/Form/XpType.php
@@ -6,19 +6,26 @@ use App\Entity\Xp;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 
 class XpType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('jobDateFrom')
-            ->add('jobDateTo')
+            ->add('jobDateFrom',DateType::class, [
+                'widget' => 'single_text',
+                'placeholder' => 'dd/mm/aaaa',  
+            ])
+            ->add('jobDateTo',DateType::class, [
+                'widget' => 'single_text',
+                'placeholder' => 'dd/mm/aaaa',  
+            ])
             ->add('jobName')
             ->add('companyName')
             ->add('jobLocation')
             ->add('jobDescription')
-            ->add('userId')
+            // ->add('userId')
         ;
     }
 
