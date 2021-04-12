dbc2debec46d316cbc9913971c078599c085ae90
swann-martin <swannmartin1988@gmail.com>
2021-04-12 11:10:53 +0200

    changed page edit CV and its form

--- src/Controller/CvController.php
+++ src/Controller/CvController.php
@@ -25,7 +25,7 @@ class CvController extends AbstractController
         if (!$this->getUser()) {
             return $this->redirectToRoute('app_login');
         }
-        
+
         return $this->render('cv/index.html.twig', [
             'cvs' => $cvRepository->findAll(),
         ]);
@@ -45,22 +45,22 @@ class CvController extends AbstractController
         // if ($form->isSubmitted() && $form->isValid()) {
 
 
-            $user = $security->getUser();
-            if (count($this->getUser()->getCvs()) < 3) {
+        $user = $security->getUser();
+        if (count($this->getUser()->getCvs()) < 3) {
 
-                $cv->setUserId($user);
-                $cv->setModel(1);
-                $cv->setTitle('Définis le titre de ton cv');
-                $cv->setJobCv('Inscris ton poste visé');
-                $cv->setAbout('Décris ton profil');
-                $cv->setShortUrl('');
-        
-                $entityManager = $this->getDoctrine()->getManager();
-                $entityManager->persist($cv);
-                $entityManager->flush();
-            }
+            $cv->setUserId($user);
+            $cv->setModel(1);
+            $cv->setTitle('Définis le titre de ton cv');
+            $cv->setJobCv('Inscris ton poste visé');
+            $cv->setAbout('Décris ton profil');
+            $cv->setShortUrl('');
 
-            return $this->redirectToRoute('cv_index');
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($cv);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('cv_index');
         // }
 
         return $this->render('cv/new.html.twig', [
@@ -77,10 +77,18 @@ class CvController extends AbstractController
         if (!$this->getUser() || $this->getUser()->getId() != $cv->getUserId()->getId()) {
             return $this->redirectToRoute('app_login');
         }
-       $session = $request->getSession();
-       $session->set('id',$id);   
+        $session = $request->getSession();
+        $session->set('id', $id);
+
+        $shortUrl = '/user/' . $this->getUser()->getId() . '/' . $id;
+        $cv->setShortUrl($shortUrl);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($cv);
+        $entityManager->flush();
+
         return $this->render('cv/show.html.twig', [
-            'cv' => $cv,       
+            'cv' => $cv,
         ]);
     }
 
@@ -113,7 +121,7 @@ class CvController extends AbstractController
      */
     public function delete(Request $request, Cv $cv): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$cv->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $cv->getId(), $request->request->get('_token'))) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->remove($cv);
             $entityManager->flush();
@@ -135,6 +143,6 @@ class CvController extends AbstractController
         $entityManager = $this->getDoctrine()->getManager();
         $entityManager->flush();
 
-        return $this->redirectToRoute('cv_show', ['id'=> $id]);
+        return $this->redirectToRoute('cv_show', ['id' => $id]);
     }
 }
