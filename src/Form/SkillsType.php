f4020fd855e3758f89580746d2d0f02f8f76b8e2
Michael Krysztofiak <mkl@iMac-de-Michael.local>
2021-04-14 11:23:24 +0200

    integration api

--- src/Form/SkillsType.php
+++ src/Form/SkillsType.php
@@ -13,29 +13,7 @@ class SkillsType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-        ->add('icon', ChoiceType::class, [
-            'placeholder' => 'Choisis une icone',
-            'choices'  => [
-                'Logo js' => 'fab fa-js',
-                'Logo php' => 'fab fa-php',
-                'Logo Symfony' => 'fab fa-symfony',
-                'Logo Angular' => 'fab fa-angular',
-                'Logo Vue-JS' => 'fab fa-vuejs',
-                'Logo Python' => 'fab fa-python',
-                'Logo bootstrap' => 'fab fa-bootstrap',
-                'Logo wordpress' => 'fab fa-wordpress',
-                'Logo node-js' => 'fab fa-node',
-                'Logo html' => 'fab fa-hml5',
-                'Logo css' => 'fab fa-css3-alt',
-                'Logo sass' => 'fab fa-sass',
-                'Logo SQL' => 'fab fas-database',
-                'Logo Github' => 'fab fa-github',
-                'Logo shell' => 'fas fa-terminal',
-                
-            ],
-        ]
-        
-        )
+        ->add('icon')
             ->add('skillName')
             // ->add('userId')
         ;
