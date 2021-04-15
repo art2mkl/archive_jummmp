824503c447ed366d91fd3a53cc13a000534bdc74
Michael Krysztofiak <mkl@iMac-de-Michael.local>
2021-04-15 15:55:00 +0200

    annexs + cvcreateCSS

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -33,4 +33,35 @@ class SecurityController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+    /**
+     * @Route("/annex/policy", name="app_policy")
+     */
+    public function policy()
+    {
+        return $this->render('annex/policy.html.twig'); 
+    }
+    /**
+     * @Route("/annex/plan", name="app_plan")
+     */
+    public function plan()
+    {
+        return $this->render('annex/plan.html.twig'); 
+    }
+    /**
+     * @Route("/annex/cg", name="app_cg")
+     */
+    public function cg()
+    {
+        return $this->render('annex/cg.html.twig'); 
+    }
+    /**
+     * @Route("/annex/faq", name="app_faq")
+     */
+    public function faq()
+    {
+        return $this->render('annex/faq.html.twig'); 
+    }
+
+
+
 }
